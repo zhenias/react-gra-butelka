ad5bdd0d285455a316ce5b327b4d0e2fd4dfe3a5
zhenias <jekasamarash@gmail.com>
2025-07-26 22:37:23 +0200

    feat: update dependencies and add MUI theme provider
    - Added @emotion/react, @emotion/styled, and @mui/material dependencies.
    - Updated package.json to include new dependencies and reorganized devDependencies.
    - Refactored layout.tsx to remove unused font imports and update metadata.
    - Simplified Home component in page.tsx to use ThemeProvider and Processing components.
    - Created .env.example for environment variable setup.
    - Implemented ThemeProvider component with custom MUI theme and global styles.
    - Added Processing component to display a loading indicator.
    - Created empty components for RandomQuestion, SelectPlayers, and SelectTopic screens.

---
+++ src/app/components/Context/Theme.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { FunctionComponent, ReactNode } from 'react';
+import { createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+import { GlobalStyles } from '@mui/material';
+
+const theme = createTheme({
+    palette: {
+        text: {
+            primary: '#202020'
+        },
+        primary: {
+            main: '#00bcd4',
+            contrastText: '#fff'
+        },
+        secondary: {
+            main: '#ff9800',
+            contrastText: '#fff'
+        },
+    },
+    typography: {
+        fontSize: 13
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: ({ ownerState }) => ({
+                    borderRadius: '2px',
+                    fontSize: '14px',
+                    fontWeight: 600,
+                    boxShadow: 'none',
+                    '&:hover': {
+                        boxShadow: 'none',
+                    },
+                    lineHeight: 1.5,
+                    ...(ownerState.size === 'small' && {
+                        fontSize: '12px'
+                    }),
+                }),
+            }
+        },
+        MuiTypography: {
+            styleOverrides: {
+                root: {
+                    fontSize: '14px',
+                    letterSpacing: '0.00938em'
+                }
+            }
+        },
+        MuiDialog: {
+            styleOverrides: {
+                paper: {
+                    borderRadius: '0px',
+                    boxShadow: 'none'
+                }
+            }
+        },
+        MuiDialogTitle: {
+            styleOverrides: {
+                root: {
+                    font: '600 18px/24px "Roboto", sans-serif',
+                    paddingBottom: 0
+                }
+            }
+        },
+        MuiDialogContent: {
+            styleOverrides: {
+                root: {
+                    paddingBottom: 8
+                }
+            }
+        },
+        MuiDialogContentText: {
+            styleOverrides: {
+                root: {
+                    color: '#333',
+                    fontSize: '15px',
+                    lineHeight: '18px'
+                }
+            }
+        },
+        MuiTooltip: {
+            styleOverrides: {
+                tooltip: {
+                    background: '#fafafa',
+                    color: '#111',
+                    fontSize: '12px',
+                    borderRadius: '3px',
+                    border: '1px solid #e1e1e1',
+                    boxShadow: '2px 2px 8px rgba(0, 0, 0, 0.4)'
+                }
+            }
+        },
+        MuiTextField: {
+            defaultProps: {
+                variant: 'standard'
+            }
+        }
+    }
+});
+
+const ThemeProvider: FunctionComponent<{ children: ReactNode }> = ({ children }) => {
+    return (
+        <MuiThemeProvider theme={theme}>
+            <CssBaseline />
+            <GlobalStyles
+                styles={{
+                    '.MuiButtonBase-root.MuiPickersDay-root:focus.Mui-selected': {
+                        backgroundColor: '#00bcd4'
+                    },
+                    '.MuiButtonBase-root.MuiPickersDay-root.MuiPickersDay-today:not(.Mui-selected)': {
+                        border: 'none',
+                        color: '#00bcd4',
+                        backgroundColor: 'transparent',
+                        fontWeight: 600
+                    }
+                }}
+            />
+            {children}
+        </MuiThemeProvider>
+    );
+};
+
+export default ThemeProvider;
---
+++ src/app/components/Processing/Processing.tsx
@@ -0,0 +1,10 @@
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+
+export default function Processing() {
+    return (
+        <>
+            <CircularProgress color="secondary" />
+        </>
+    );
+}
