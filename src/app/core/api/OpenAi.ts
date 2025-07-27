d9c46a0dd13bf66ae4729e02ceddf8abc9624142
zhenias <jekasamarash@gmail.com>
2025-07-27 16:20:34 +0200

    -

--- src/app/core/api/OpenAi.ts
+++ src/app/core/api/OpenAi.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 export default async function OpenAIResponse() {
     const client = new OpenAI({
-        apiKey: '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',
+        apiKey: '',
         dangerouslyAllowBrowser: true,
     });
 
